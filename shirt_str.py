0399476edf3daf5910f53d794e05a0ed2d726cab
robpotter89 <robert.potter@numerator.com>
2019-10-01 16:58:02 -0400

    palindrome shirt

---
+++ shirt_str.py
@@ -0,0 +1,28 @@
+def is_palindrome(str):
+    start_i = 0
+    end_i = len(str) - 1
+    while start_i < end_i:
+        if str[start_i] == str[end_i]:
+            start_i += 1
+            end_i -= 1
+        else:
+            return False
+    return True
+
+def recursive_palindrome(str):
+    if len(str) <= 1:
+        return True
+    if str[0] == str[len(str) - 1]:
+        return recursive_palindrome(str[1:-1])
+    else:
+        return False
+
+
+def main():
+    str = 'shirt'
+    print(recursive_palindrome(str.lower()))
+    print(is_palindrome(str.lower()))
+
+
+if __name__ == '__main__':
+    main()
